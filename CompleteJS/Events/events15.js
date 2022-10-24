51815ceee48898a8396974baa0aacda73a3ae3af
BhupeshXD <bhupeshdhapola7@gmail.com>
2022-10-24 09:42:24 +0530

    24 oct

---
+++ CompleteJS/Events/events15.js
@@ -0,0 +1,22 @@
+// Events 
+
+// html events are things that happes to html Elements
+
+// when javascript is used in html pages, javascript can react on these Events
+
+// an html events can be something the browser does, or something a user does
+
+
+
+// there are 4 types to write events in javascript are :
+
+// using inline event alert();
+// by calling a function
+// using inline events (Hyml onclick=** property and element.onclick)
+// using event listener (addEventListener and IE's attachEvent)
+
+
+
+// what is event object?
+
+
