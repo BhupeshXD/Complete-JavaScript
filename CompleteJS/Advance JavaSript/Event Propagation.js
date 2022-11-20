ae29599694c6f73f00b0d7b1f38f196a77fcdbe9
BhupeshXD <bhupeshdhapola7@gmail.com>
2022-11-20 20:19:40 +0530

    20 nov

---
+++ CompleteJS/Advance JavaSript/Event Propagation.js
@@ -0,0 +1,32 @@
+// The event propagation mode determines in which order the elements receive the event
+
+
+// Event bubling and capturing are two ways of event propagati in the HTML DOM API, when an event occurs in a element inside another element, and both elements have regsitered a handle for that event
+
+// event propagation mode determines in which order the Elements receive thr event 
+
+// if you go from top to Bottom it is capturing face 
+
+// if you go from bottom to top it is bubbling face 
+
+
+// capture phase
+
+
+
+// target phase
+
+
+
+// bubble phase 
+
+
+
+
+// Event Bubbling
+
+// the event is first captured and handled by the innermost Element and then propagated to outer Elements 
+
+// Event Capturing 
+
+// the event first capture by the outermost Element and propagated to the inner elements 
